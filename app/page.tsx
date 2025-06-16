f89053ffd068ac0b3ba39a30f2e1052c80fc05ed
PIPO <kenlyisdog@gmail.com>
2025-06-16 19:18:19 +0700

    fix: update instruction text in Home component for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly 11111.
+            Save and see your changes instantly 0932473600.
           </li>
         </ol>
 
